cbe93476d2e517efc81065c160cfc1fe3e9d7632
mohamed taha93 <mohamedtahadawoud@gmail.com>
2023-05-05 10:00:10 +0300

    modify Socket client and make navigation in Sign Up

--- src/controller/SignBase.java
+++ src/controller/SignBase.java
@@ -73,7 +73,7 @@ public  class SignBase extends AnchorPane {
         btnSignUp.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                signUpBase signUpRoot=new signUpBase();
+                signUpBase signUpRoot=new signUpBase( primaryStage);
                 Scene signUpScene = new Scene(signUpRoot);
                 signUpScene.getStylesheets().add("file:./src/Photo/buttonStyle.css");
                 primaryStage.setScene(signUpScene);
